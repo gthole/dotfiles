75ada6f8e4c80b7fa7bb758ba9a35ecb33516cb1
Greg Thole <gthole@gmail.com>
2022-02-15 15:01:52 -0500

    Add password generation function

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ fi
 brew update
 
 # Install!
-brew install fish tmux git watch wget htop python3 node ag pass
+brew install fish tmux git watch wget htop python3 node ag pass pwgen
 # brew install imagemagick libpng jpeg libtiff
 # brew install reattach-to-user-namespace
 
